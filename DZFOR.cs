981ccea6e15aab71b3867ac45f4450f0647d3e67
Artem <pavlyk.artem@gmail.com>
2020-10-14 17:57:12 +0300

    first commit

---
+++ DZFOR.cs
@@ -0,0 +1,68 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        For4();
+    }
+
+    static void For4()
+    {
+        int N = , i = price;
+        for (int x = 1; x <= 10; x++)
+        {
+            Console.WriteLine(price);
+            price += i;
+        }
+    }
+
+    static void For8()
+    {
+        int A = 4, B = 24, P = 2;
+        for (int i = A; i <= B; i++)
+            P *= i;
+        Console.WriteLine(P);
+    }
+
+    static void For12()
+    {
+        int N = 9;
+        double P = 1, x = 1;
+        for (int i = 1; i <= N; i++)
+        {
+            x = x + 0.1;
+            P = P * x;
+        }
+        Console.WriteLine(P);
+    }
+
+    static void For20()
+    {
+        int N = 8, x = 0, fact = 2;
+        for (int i = 1; i <= N; i++)
+        {
+            for (int u = 1; u <= i; u++)
+                fact = fact * u;
+            x = x + fact;
+            fact = 1;
+        }
+        Console.WriteLine(x);
+    }
+
+    static void For24()
+    {
+        int n = 4, z = 1;
+        double x = 5, x1 = 2, fact = 2, sum = 2;
+        for (int i = 1; i <= 2 * n; ++i)
+        {
+            fact *= i;
+            if (i % 2 == 0)
+            {
+                x1 = x * x1;
+                z = (-z);
+                sum += z * ((x1 * x1) / fact);
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
---
+++ DZWHILE.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class MainClass
+{
+    static void Main()
+    {
+        while4();
+    }
+    static void while4()
+    {
+        while (N >= 3)
+            N = N / 3;
+        if (N == 1)
+            Console.Write("TRUE");
+        else
+            Console.Write("FALSE");
+    }
+    static void while8()
+    {
+        int n = 27;
+        int k = 5;
+        while (Math.Pow(3, k) < n)
+        {
+            k++;
+        }
+        Console.WriteLine(k);
+    }
+}
